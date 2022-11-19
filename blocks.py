8b1dbe844cdf0300660420c2a8b6420d6ba55136
ritesh-blog27 <riteshminecraft27@gmail.com>
2022-11-19 14:41:03 +0530

    Update 1

--- blocks.py
+++ blocks.py
@@ -1,11 +1,12 @@
 from turtle import Turtle
+from random import choices
 
 class Block(Turtle):
 
     def __init__(self, position):
         super().__init__()
-
-        self.color('red')
+        colors = ['red', 'yellow', 'green', 'blue', 'powder blue', 'grey', 'violet', 'indigo', 'pink']
+        self.color(choices(colors))
         self.shape('square')
         # self.turtlesize((20.0, 30.0))
         self.speed(10)
--- main.py
+++ main.py
@@ -5,6 +5,23 @@ screen = turtle.Screen()
 screen.bgcolor('black')
 screen.screensize(600, 600)
 
+wt = turtle.Turtle()
+
+score = 0
+
+def is_collided_with(a, b):
+    return abs(a.xcor() - b.xcor()) < 10 and abs(a.ycor() - b.ycor()) < 10
+
+def move_right():
+    player.fd(5)
+
+def move_left():
+    player.backward(5)
+
+screen.onkeypress(move_left, 'Left')
+screen.onkeypress(move_right, 'Right')
+screen.listen()
+
 position = [(-300, 300), (-270, 300), (-240, 300), (-210, 300), (-180, 300), (-150, 300), (-120, 300), (-90, 300), (-60, 300), (-30, 300), 
             (0, 300), (30, 300), (60, 300), (90, 300),  (120, 300), (150, 300), (180, 300), (210, 300), (240, 300), (270, 300), (300, 300),
             (-300, 275), (-270, 275), (-240, 275), (-210, 275), (-180, 275), (-150, 275), (-120, 275), (-90, 275), (-60, 275), (-30, 275), (0, 275),
@@ -13,23 +30,69 @@ position = [(-300, 300), (-270, 300), (-240, 300), (-210, 300), (-180, 300), (-1
             (0, 250), (30, 250), (60, 250), (90, 250),  (120, 250), (150, 250), (180, 250), (210, 250), (240, 250), (270, 250), (300, 250)]
 
 blocks = []
-position = [(0,0)]
+# position = [(0,0)]
+
+turtle.tracer(False)
 for i in position:
     tt = Block(i)
     blocks.append(tt)
-
-print(blocks)
+turtle.tracer(True)
 
 ball = turtle.Turtle('circle')
-ball.speed(2)
+ball.speed(1.5)
+ball.penup()
+ball.goto(0, -300)
+ball.setheading(145)
 ball.color('white')
-ball.seth(180)
+
+player = turtle.Turtle('square')
+player.goto(0, -300)
+player.penup()
+player.color('white')
+player.resizemode('user')
+player.shapesize(2, 8, 2)
+
 game_on = True
 while game_on:
     ball.fd(10)
     x, y = ball.pos()
-    print(x, y)
+    for i in blocks:
+        if is_collided_with(ball, i):
+            score += 1
+            if ball.heading() > 0 and ball.heading() < 90:
+                i.hideturtle()
+                i.goto(-500, 300)
+                ball.seth(315)
+            else:
+                i.hideturtle()
+                ball.seth(225)
+    print(player.pos())
+    if abs(ball.xcor() - player.xcor()) < 70 and abs(ball.ycor() - player.ycor()) < 16:
+        if ball.heading() > 90 and ball.heading() < 270:
+            ball.setheading(135)
+        else:
+            ball.setheading(45)
+
     if x > 320.0:
-        continue
+        print(ball.heading())
+        if ball.heading() > 0.0 and ball.heading() <180.0:
+            ball.setheading(135)
+        else:
+            ball.setheading(225)
+    if y > 320.0:
+        break
+    if x < -320.0:
+        if ball.heading() > 0.0 and ball.heading() <180.0:
+            ball.seth(45)
+        else:
+            ball.seth(315)
+    if y < -340.0:
+        
+        wt.goto(-25,0)
+        wt.pencolor('green')
+        wt.hideturtle()
+        wt.write(f'GAME Over\nScore : {score}', font=['Times', 30])
+        
+        break
 
 screen.mainloop()
